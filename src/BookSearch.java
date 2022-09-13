142821bf85d294602e2be482571c5f5991ebdd54
ubaid <ubaidansari4547@gmail.com>
2022-09-13 20:54:57 +0530

    seventh commit

--- src/BookSearch.java
+++ src/BookSearch.java
@@ -7,13 +7,18 @@
  *
  * @author ubaid
  */
+import java.sql.*;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 public class BookSearch extends javax.swing.JFrame {
 
     /**
      * Creates new form BookSearch
      */
+    String op;
     public BookSearch() {
         initComponents();
+        name.setVisible(false);
     }
 
     /**
@@ -25,12 +30,15 @@ public class BookSearch extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        mess = new javax.swing.JLabel();
+        name = new javax.swing.JTextField();
+        combo = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table = new javax.swing.JTable();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setText("Book search");
 
         jButton1.setText("<- Back");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -39,27 +47,75 @@ public class BookSearch extends javax.swing.JFrame {
             }
         });
 
+        mess.setText("Please select catagory");
+
+        combo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Book", "Author", "Publisher" }));
+        combo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                comboMouseClicked(evt);
+            }
+        });
+        combo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comboActionPerformed(evt);
+            }
+        });
+
+        table.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Book Id", "Book Name", "Author Name", "Publisher Name", "Quantity Available", "Price"
+            }
+        ));
+        table.setRowMargin(2);
+        jScrollPane1.setViewportView(table);
+
+        jButton2.setText("Search");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(149, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(169, 169, 169))
             .addGroup(layout.createSequentialGroup()
-                .addGap(74, 74, 74)
-                .addComponent(jButton1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(15, 15, 15)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton2)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 929, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(mess, javax.swing.GroupLayout.DEFAULT_SIZE, 235, Short.MAX_VALUE)
+                            .addComponent(name))
+                        .addGap(48, 48, 48)
+                        .addComponent(combo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(71, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(117, 117, 117)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 120, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(16, 16, 16)
+                        .addComponent(combo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(mess, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addGap(34, 34, 34)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 254, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addGap(21, 21, 21))
+                .addContainerGap())
         );
 
         pack();
@@ -69,6 +125,72 @@ public class BookSearch extends javax.swing.JFrame {
         new Home().setVisible(true);        // TODO add your handling code here:
 dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    public boolean check(String main,String sub){
+        int c=0;
+        for(int i=0;i<sub.length();i++){
+            for(int j=i;j<main.length();j++){
+                if(sub.charAt(i)==main.charAt(j)){
+                    c++;
+                    break;
+                }
+                    
+            }
+        }
+        return sub.length()==c;
+    }
+    private void comboActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboActionPerformed
+      
+        op=combo.getSelectedItem().toString();
+        mess.setText(op);
+        name.setVisible(true);
+    }//GEN-LAST:event_comboActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        
+        Connection conn=null;
+        PreparedStatement ps=null;
+        ResultSet rs=null;
+        int i;
+        try{
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/library","ubaid","root");
+            ps=conn.prepareStatement("select * from books");
+            if(op.equals("Book"))
+                i=2;
+            else if(op.equals("Author"))
+                i=3;
+            else
+                i=4;
+            rs=ps.executeQuery();
+            int count=table.getRowCount();
+            DefaultTableModel tb=(DefaultTableModel) table.getModel();
+            boolean f=false;
+            String[] item=new String[6];
+            while(rs.next()){
+                if(this.check(rs.getString(i), name.getText())){
+                    f=true;
+                
+               item[0]=rs.getString(1);
+                item[1]=rs.getString(2);
+                item[2]=rs.getString(3);
+                item[3]=rs.getString(4);
+                item[4]=rs.getString(5);
+                item[5]=rs.getString(6);
+                tb.addRow(item);
+                }
+            }
+            if(!f)
+                JOptionPane.showMessageDialog(rootPane, "No record found");
+        }
+        catch(Exception e){
+           e.printStackTrace();
+        }
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void comboMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_comboMouseClicked
+
+    }//GEN-LAST:event_comboMouseClicked
 
     /**
      * @param args the command line arguments
@@ -106,7 +228,12 @@ dispose();
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> combo;
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel mess;
+    private javax.swing.JTextField name;
+    private javax.swing.JTable table;
     // End of variables declaration//GEN-END:variables
 }
